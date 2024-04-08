0730288b37a6d5d52c4c9ef0b1d793b89f827856
AyomideKayode <ayomidekay7@gmail.com>
2024-04-08 22:27:07 +0100

    Const or Let?🚧

---
+++ 0x00-ES6_basic/0-constant.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+/* eslint-disable */
+
+// Constants are block-scoped, much like variables defined using the let statement.
+// The value of a constant cannot change through re-assignment, and it can't be redeclared.
+
+export function taskFirst() {
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+  return combination;
+}
