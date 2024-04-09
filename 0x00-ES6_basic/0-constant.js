7261563e8794a16a8b86d1c1e62c0e4b17c2d58e
AyomideKayode <ayomidekay7@gmail.com>
2024-04-09 10:42:42 +0100

    Fix unnecessary eslint-disable comment in 0-constant.js. Checker Issues, LOL🚧

--- 0x00-ES6_basic/0-constant.js
+++ 0x00-ES6_basic/0-constant.js
@@ -1,8 +1,3 @@
-#!/usr/bin/node
-
-// Constants are block-scoped, much like variables defined using the let statement.
-// The value of a constant cannot change through re-assignment, and it can't be redeclared.
-
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
