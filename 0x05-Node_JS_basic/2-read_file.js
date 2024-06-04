c005ba9d68864f17e60d8d61be018883276812fd
[AyomideKayode] <[ayomidekay7@gmail.com]>
2024-06-04 21:18:33 +0100

    Fix ES-Lint missing trailing comma.🚧

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -37,8 +37,8 @@ function countStudents(path) {
     for (const [field, names] of Object.entries(fieldCount)) {
       console.log(
         `Number of students in ${field}: ${names.length}. List: ${names.join(
-          ', '
-        )}`
+          ', ',
+        )}`,
       );
     }
   } catch (error) {
